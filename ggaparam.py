00288b02e4aec7989509eb99e85193d85a8408d0
Jiyan He <volltin@live.com>
2021-05-30 10:15:31 +0800

    fix

--- ggaparam.py
+++ ggaparam.py
@@ -4,19 +4,19 @@ import dataclasses
 @dataclasses.dataclass
 class GGAParameters:
     # Survey population size
-    Ps = 50
+    Ps: int = 50
     # Evolution population size
-    Pe = 20
+    Pe: int = 20
     # Probability of mutation
-    Pm = .04
+    Pm: float = .04
     # Probability of crossover
-    Pc = .8
+    Pc: float = .8
     # Number of survey generations
-    Gs = 20
+    Gs: int = 20
     # Number of evolution generations
     # Stopping condition: when the number of iterations reaches Ge
     # Ge = 50
-    Ge = 160
+    Ge: int = 160
 
     # will be selected based on the experiments, [0,1], 0.3
-    alpha = 0.4
+    alpha: float = 0.4
